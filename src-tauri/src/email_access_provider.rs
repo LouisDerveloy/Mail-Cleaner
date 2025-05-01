66c53757cf2d650d8d54fb479413ab29bd93330c
LouisDerveloy <louis.derveloy@gmail.com>
2025-05-01 23:58:05 +0200

    Implemented default behavior for email_access_provider module but still have error to take care

--- src-tauri/src/email_access_provider.rs
+++ src-tauri/src/email_access_provider.rs
@@ -4,6 +4,45 @@ use imap;
 use imap::Session;
 use native_tls;
 use native_tls::TlsStream;
+use serde::Serialize;
+
+pub struct OAuthCredentials {
+    user: String,
+    access_token: String,
+}
+
+impl OAuthCredentials {
+    pub fn new(user: String, access_token: String) -> OAuthCredentials {
+        OAuthCredentials { user, access_token }
+    }
+}
+
+impl imap::Authenticator for OAuthCredentials {
+    type Response = String;
+    #[allow(unused_variables)]
+    fn process(&self, data: &[u8]) -> Self::Response {
+        format!(
+            "user={}\x01auth=Bearer {}\x01\x01",
+            self.user, self.access_token
+        )
+    }
+}
+
+pub struct PasswordCredentials {
+    user: String,
+    password: String,
+}
+
+impl PasswordCredentials {
+    pub fn new(user: String, password: String) -> PasswordCredentials {
+        PasswordCredentials { user, password }
+    }
+}
+
+pub enum Credentials {
+    Password(PasswordCredentials),
+    Oauth(OAuthCredentials),
+}
 
 #[derive(Clone)]
 pub struct MailServer {
@@ -13,10 +52,7 @@ pub struct MailServer {
 
 impl MailServer {
     pub fn new(domain: String, port: u16) -> MailServer {
-        MailServer {
-            domain,
-            port,
-        }
+        MailServer { domain, port, }
     }
 }
 
@@ -30,38 +66,81 @@ pub struct EmailAccessProvider {
     imap_session: Session<TlsStream<TcpStream>>,
 }
 
+#[derive(Serialize)]
+pub struct Sender {
+    name: Option<String>,
+    email: String,
+}
+
 impl EmailAccessProvider {
-    pub fn new<E>(mail_server: MailServer, email: &str, password: &str) -> Result<Self, E>
+    pub fn new<E>(mail_server: MailServer, credentials: Credentials) -> Result<Self, E>
     where E: Display + Debug {
+        let tls = native_tls::TlsConnector::builder().build().unwrap();
 
-        let tls = native_tls::TlsConnector::builder().build()?; // Opening
+        let client = imap::connect((mail_server.domain.clone(), 993), &mail_server.domain, &tls).unwrap();
 
-        let client = imap::connect(
-            (mail_server.domain.clone(), mail_server.port.clone()),
-            &mail_server.domain,
-            &tls,
-        )?;
-
-        let mut imap_session = client.login(email, password)?;
+        // Login to the server based on what credentials where chosen by the user.
+        let mut imap_session: Session<TlsStream<TcpStream>> = match credentials {
+            Credentials::Password(credentials) => client.login(credentials.user, credentials.password).unwrap(),
+            Credentials::Oauth(credentials) => client.authenticate("XOAUTH2", &credentials, ).unwrap(),
+        };
 
 
-        Ok(Self {
-            mail_server,
+        Ok(EmailAccessProvider {
             imap_session,
+            mail_server,
         })
     }
 }
 
 impl EmailProvider for EmailAccessProvider {
-    async fn get_inbox_senders_email_list<E>(&mut self) -> Result<Vec<String>, E>
+    async fn get_inbox_senders_email_list<E>(&mut self) -> Result<Vec<Sender>, E>
     where E: Display + Debug {
 
-        let messages = self.imap_session.fetch("1", "HEADER").unwrap();
-        if let Some(m) = messages.iter().next() {
-            Ok(vec![m.message.to_string()])
-        } else {
-            Ok(Vec::new())
+        let mut senders: Vec<Sender> = Vec::new();
+
+        // Search for all the ids of emails which match the request <BODY unsubscribe> that mean the body has to contain the word unsubscribe
+        for seq in self.imap_session.search("BODY unsubscribe").unwrap().iter() {
+
+            // Fetch the sender of those mails
+            match self.imap_session.fetch(seq.to_string(), "ALL") {
+                Ok(msgs) => {
+                    msgs.iter().for_each(|msg| {
+
+                        if let Some(envelop) = msg.envelope() {
+                            if let Some(_senders) = &envelop.sender {
+                                _senders.iter().for_each(|sender| {
+
+                                    // Check if mailbox and host are defined
+                                    if let (Some(mailbox), Some(host)) = (sender.mailbox, sender.host) {
+
+                                        senders.push(
+                                            Sender {
+                                                name: sender.name.map(|name| String::from_utf8(name.to_owned()).expect("Failed to convert &[u8] of the name to String")), // .map convert Option(&[u8]) to Option(String)
+                                                email: format!(
+                                                    "{}@{}",
+                                                    String::from_utf8(mailbox.to_vec()).expect("Failed to convert &[u8] of the mailbox to String"),
+                                                    String::from_utf8(host.to_vec()).expect("Failed to convert &[u8] of the host to String")
+                                                ), // Concatenate the mailbox and the host into a real mail
+                                            }
+                                        );
+
+                                    }
+
+                                })
+                            }
+                        }
+
+                    })
+                }
+                Err(err) => {
+                    println!("Couldn't fetch\nError: {:?}", err);
+                }
+            }
         }
+
+        Ok(vec![])
+
     }
 }
 
--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -2,151 +2,116 @@
 use tauri::ipc::{Channel, IpcResponse};
 
 use std::fmt::{Debug, Display};
+use std::net::TcpStream;
 use imap;
 use native_tls;
 use base64;
-// use crate::email_access_provider::EmailProvider;
-// mod email_access_provider;
+use imap::Session;
+use native_tls::TlsStream;
+use crate::email_access_provider::{EmailAccessProvider, EmailProvider, MailServer, OAuthCredentials, Sender};
+mod email_access_provider;
 
-
-struct GmailOAuth2 {
-    user: String,
-    access_token: String,
-}
-
-impl imap::Authenticator for GmailOAuth2 {
-    type Response = String;
-    #[allow(unused_variables)]
-    fn process(&self, data: &[u8]) -> Self::Response {
-        format!(
-            "user={}\x01auth=Bearer {}\x01\x01",
-            self.user, self.access_token
-        )
-    }
+enum CommandResult {
+    Success,
+    Failure(String),
 }
 
 
 #[tauri::command]
-async fn get_list(ret_channel: Channel<[i32; 10]>) {
+async fn get_list(ret_channel: Channel<Sender>) -> CommandResult {
 
-    // let mut email_provider = email_access_provider::EmailAccessProvider::new(
-    //     email_access_provider::MailServer::new(String::from("imap.gmail.com"), 993),
-    //     "louis.derveloy@gmail.com",
-    //     "qsdfjl"
-    // ).expect("Failed to initialize email provider");
+    // let gmail_auth = email_access_provider::OAuthCredentials::new(
+    //     String::from("louis.derveloy.gta5@gmail.com"),
+    //     String::from("ya29.a0AZYkNZh7GX2BSTgksTcgZTXENVXw8KVbUi_3vwSdoxrEc1krnaKph5GIhPaen_os_tkieM8VsVcqtMuvXq2gA2v_ZERqySS_32a8LO238R5KQ8izg9HPFZr8tqRMqwCiBTLY5jYS4xE3E-5aerS9I5TOmF1iGQxhMrrVqYPkaCgYKASESARESFQHGX2Mi-Nv9o8CIB3TexAx4jFkxTg0175"),
+    // );
     //
-    // match email_provider.get_inbox_senders_email_list().await {
-    //     Ok(msg) => {
-    //         println!("Email fetched msg = {}", msg.first().unwrap());
-    //     }
-    //     Err(e_msg) => {
-    //         println!("Error getting inbox senders email list: {}", e_msg);
-    //     }
+    // let domain = "imap.gmail.com";
+    // let tls = native_tls::TlsConnector::builder().build().unwrap();
+    //
+    // // we pass in the domain twice to check that the server's TLS
+    // // certificate is valid for the domain we're connecting to.
+    // let client = imap::connect((domain, 993), domain, &tls).unwrap();
+    //
+    // // the client we have here is unauthenticated.
+    // // to do anything useful with the e-mails, we need to log in
+    //
+    // let mut imap_session = client.authenticate(
+    //     "XOAUTH2",
+    //     &gmail_auth,
+    // ).expect("Authentication failed");
+    //
+    //
+    //
+    // for name in imap_session.list(None, None).expect("List failed").iter() {
+    //     println!("Name: {:?}", name);
     // };
-
-    let gmail_auth = GmailOAuth2 {
-        user: String::from("louis.derveloy.gta5@gmail.com"),
-        access_token: String::from("ya29.a0AZYkNZh7GX2BSTgksTcgZTXENVXw8KVbUi_3vwSdoxrEc1krnaKph5GIhPaen_os_tkieM8VsVcqtMuvXq2gA2v_ZERqySS_32a8LO238R5KQ8izg9HPFZr8tqRMqwCiBTLY5jYS4xE3E-5aerS9I5TOmF1iGQxhMrrVqYPkaCgYKASESARESFQHGX2Mi-Nv9o8CIB3TexAx4jFkxTg0175"),
-    };
-
-    let domain = "imap.gmail.com";
-    let tls = native_tls::TlsConnector::builder().build().unwrap();
-
-    // we pass in the domain twice to check that the server's TLS
-    // certificate is valid for the domain we're connecting to.
-    let client = imap::connect((domain, 993), domain, &tls).unwrap();
-
-    // the client we have here is unauthenticated.
-    // to do anything useful with the e-mails, we need to log in
-
-    let mut imap_session = client.authenticate(
-        "XOAUTH2",
-        &gmail_auth,
-    ).expect("Authentication failed");
-
-
-
-    for name in imap_session.list(None, None).expect("List failed").iter() {
-        println!("Name: {:?}", name);
-    };
-
-
-    // we want to fetch the first email in the INBOX mailbox
-    let number_of_emails = imap_session.select("INBOX").expect("select failed").exists;
-    println!("Number of emails: {}", number_of_emails);
-
-
-    // Search for emails
-    for seq in imap_session.search("BODY reddit").unwrap().iter() {
-        match imap_session.fetch(seq.to_string(), "ALL") {
-            Ok(msgs) => {
-                msgs.iter().for_each(|msg| {
-
-                    if let Some(envelop) = msg.envelope() {
-                        if let Some(senders) = &envelop.sender {
-                            senders.iter().for_each(|sender| {
-
-                                println!("Mailbox: {:?}\n\n", match sender.mailbox {
-                                    None => "".to_string(),
-                                    Some(host) => String::from_utf8(host.to_vec()).unwrap(),
-                                });
-                                println!("Host: {:?}", match sender.host {
-                                    None => "".to_string(),
-                                    Some(host) => String::from_utf8(host.to_vec()).unwrap(),
-                                });
-
-                                println!("Name: {:?}", match sender.name {
-                                    None => "".to_string(),
-                                    Some(host) => String::from_utf8(host.to_vec()).unwrap(),
-                                });
-
-                            })
-                        }
-                    }
-
-                })
-            }
-            Err(err) => {
-                println!("Couldn't fetch\nError: {:?}", err);
-            }
-        }
-    }
     //
     //
-    // // fetch message number 1 in this mailbox, along with its RFC822 field.
-    // // RFC 822 dictates the format of the body of e-mails
-    // for i in 1u8..11u8 {
-    //     println!("Fetching i={}", i);
+    // // we want to fetch the first email in the INBOX mailbox
+    // let number_of_emails = imap_session.select("INBOX").expect("select failed").exists;
+    // println!("Number of emails: {}", number_of_emails);
     //
-    //     match imap_session.fetch(i.to_string(), "[Gmail]/All Mail") {
+    //
+    // // Search for emails
+    // for seq in imap_session.search("BODY reddit").unwrap().iter() {
+    //     match imap_session.fetch(seq.to_string(), "ALL") {
     //         Ok(msgs) => {
-    //             for msg in msgs.iter() {
-    //                 println!("Email i={} msg: {:?}", i, msg.message);
-    //                 println!("Email i={} msg: {:?}", i, msg.message.body().unwrap().deserialize::<i32>().unwrap());
-    //             }
-    //         }
-    //         Err(e) => println!("Error Fetching email {}: {}", i, e),
-    //     };
+    //             msgs.iter().for_each(|msg| {
     //
-    //     std::thread::sleep(std::time::Duration::from_secs(2));
+    //                 if let Some(envelop) = msg.envelope() {
+    //                     if let Some(senders) = &envelop.sender {
+    //                         senders.iter().for_each(|sender| {
+    //
+    //                             println!("Mailbox: {:?}\n\n", match sender.mailbox {
+    //                                 None => "".to_string(),
+    //                                 Some(host) => String::from_utf8(host.to_vec()).unwrap(),
+    //                             });
+    //                             println!("Host: {:?}", match sender.host {
+    //                                 None => "".to_string(),
+    //                                 Some(host) => String::from_utf8(host.to_vec()).unwrap(),
+    //                             });
+    //
+    //                             println!("Name: {:?}", match sender.name {
+    //                                 None => "".to_string(),
+    //                                 Some(host) => String::from_utf8(host.to_vec()).unwrap(),
+    //                             });
+    //
+    //                         })
+    //                     }
+    //                 }
+    //
+    //             })
+    //         }
+    //         Err(err) => {
+    //             println!("Couldn't fetch\nError: {:?}", err);
+    //         }
+    //     }
     // }
 
+    let email_session = EmailAccessProvider::new(
+        MailServer::new(
+            String::from("imap.gmail.com"),
+            993,
+        ),
+        email_access_provider::Credentials::Oauth(OAuthCredentials::new(
+            String::from("louis.derveloy.gta5@gmail.com"),
+            String::from("ya29.a0AZYkNZgbWbXjx6Rh1gUdRPXqQ8Ig7OzuZdcq1-uXBbmsk1eMbzBH8ei_MJ8yJqlVVjlgYshX_dnbeY10G-gXYQ36VY7CFVVLenxKvHWLHOMx4QDtxY8oeBne-vDLnzALl5v12Xh7Agz-i5NVOltRK-ImD1b-tkODZG_qcQqaaCgYKAdoSARESFQHGX2Mib0cDiHrGElF8DmmfcSooUQ0175"),
+        ))
+    );
 
-    let lst = [1,2,3,4,5,6,7,8,9,10];
-
-    for i in 0..150 {
-        let mut ret = lst;
-        for j in 0..10 {
-            ret[j as usize] = ret[j as usize] + i*10;
-        }
-
-        println!("Data sent");
-        std::thread::sleep(std::time::Duration::from_secs(1));
-        ret_channel.send(ret).unwrap();
+    if email_session.is_err() {
+        return CommandResult::Failure(format!("An error occurred: {}", email_session.err().unwrap()));
     }
 
+    let mut email_session = email_session.unwrap();
 
+    let senders: Vec<Sender> = email_session.get_inbox_senders_email_list().await.expect("Couldn't get senders' emails");
 
+    for sender in senders.into_iter() {
+        ret_channel.send(sender).unwrap();
+    }
+
+    CommandResult::Success
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
